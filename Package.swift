a1ababe13b91662a15937ca63f2b6c3ea3b6686c
prodbyeagle <124641014+prodbyeagle@users.noreply.github.com>
2026-07-26 22:26:15 +0200

    build(dependencies): use prodbyeagle package forks

--- Package.swift
+++ Package.swift
@@ -24,13 +24,13 @@ let package = Package(
       targets: ["Supabase", "Functions", "PostgREST", "Auth", "Realtime", "Storage"]),
   ],
   dependencies: [
-    .package(url: "https://github.com/apple/swift-crypto.git", "1.0.0"..<"5.0.0"),
-    .package(url: "https://github.com/apple/swift-http-types.git", from: "1.3.0"),
-    .package(url: "https://github.com/pointfreeco/swift-clocks", from: "1.0.0"),
-    .package(url: "https://github.com/pointfreeco/swift-concurrency-extras", from: "1.1.0"),
+    .package(url: "https://github.com/prodbyeagle/swift-crypto.git", branch: "xcode-cloud"),
+    .package(url: "https://github.com/prodbyeagle/swift-http-types.git", from: "1.3.0"),
+    .package(url: "https://github.com/prodbyeagle/swift-clocks", branch: "xcode-cloud"),
+    .package(url: "https://github.com/prodbyeagle/swift-concurrency-extras", from: "1.1.0"),
     .package(url: "https://github.com/pointfreeco/swift-custom-dump", from: "1.3.2"),
     .package(url: "https://github.com/pointfreeco/swift-snapshot-testing", from: "1.17.0"),
-    .package(url: "https://github.com/pointfreeco/xctest-dynamic-overlay", from: "1.2.2"),
+    .package(url: "https://github.com/prodbyeagle/xctest-dynamic-overlay", from: "1.2.2"),
     .package(url: "https://github.com/WeTransfer/Mocker", from: "3.0.0"),
   ],
   targets: [
