1638640c3c40bbea83221f6a6264a9c1b6e6d469
prodbyeagle <124641014+prodbyeagle@users.noreply.github.com>
2026-07-26 22:27:56 +0200

    fix(dependencies): use fork branch references

--- Package.swift
+++ Package.swift
@@ -25,12 +25,12 @@ let package = Package(
   ],
   dependencies: [
     .package(url: "https://github.com/prodbyeagle/swift-crypto.git", branch: "xcode-cloud"),
-    .package(url: "https://github.com/prodbyeagle/swift-http-types.git", from: "1.3.0"),
+    .package(url: "https://github.com/prodbyeagle/swift-http-types.git", branch: "xcode-cloud"),
     .package(url: "https://github.com/prodbyeagle/swift-clocks", branch: "xcode-cloud"),
-    .package(url: "https://github.com/prodbyeagle/swift-concurrency-extras", from: "1.1.0"),
+    .package(url: "https://github.com/prodbyeagle/swift-concurrency-extras", branch: "xcode-cloud"),
     .package(url: "https://github.com/pointfreeco/swift-custom-dump", from: "1.3.2"),
     .package(url: "https://github.com/pointfreeco/swift-snapshot-testing", from: "1.17.0"),
-    .package(url: "https://github.com/prodbyeagle/xctest-dynamic-overlay", from: "1.2.2"),
+    .package(url: "https://github.com/prodbyeagle/xctest-dynamic-overlay", branch: "xcode-cloud"),
     .package(url: "https://github.com/WeTransfer/Mocker", from: "3.0.0"),
   ],
   targets: [
